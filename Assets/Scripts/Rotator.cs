0f75593be4082e15a0bc4c74184121d7ab8515bd
Matt Krystek <mkrystek@gmail.com>
2014-01-24 00:14:19 -0800

    Removed the test script and imported my generic rotator script

---
+++ Assets/Scripts/Rotator.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotator : MonoBehaviour {
+	// Use this for initialization
+	public int rotationAngle = 45;
+	public bool rotateBackwards = false;
+
+	void Start () 
+	{
+		if( rotateBackwards == true )
+			rotationAngle = rotationAngle * -1;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		rotateObject ();
+	}
+
+	void rotateObject()
+	{
+		transform.Rotate(0,0,rotationAngle * Time.deltaTime);
+	}
+}
