47265b7bdd5a5532c174b88acd177e27a91f54b1
Matt Krystek <mkrystek@gmail.com>
2014-01-24 18:18:39 -0800

    Added two more simple utility scripts Flagged one of the lights to rotate backwards Added a simple movement script onto the camera

---
+++ Assets/Scripts/RandomPlayRate.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomPlayRate : MonoBehaviour {
+	private Animator anim;
+	public float minPlayRate;
+	public float maxPlayRate;
+
+	void Start () 
+	{
+		anim = GetComponent<Animator>();
+		anim.speed =.25f;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		float RandomDelay = Random.Range(.25f, .85f );
+		if( !IsInvoking("adjustAnimPlayRate"))
+			Invoke( "adjustAnimPlayRate", RandomDelay );
+	}
+
+	void adjustAnimPlayRate()
+	{
+		float playRate = Random.Range( minPlayRate, maxPlayRate );
+			anim.speed = playRate;
+	}
+}
---
+++ Assets/Scripts/SimpleMovement.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SimpleMovement : MonoBehaviour {
+	public float movementSpeed = .05f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey(KeyCode.W))
+			transform.position = new Vector3(transform.position.x, transform.position.y + movementSpeed, transform.position.z);
+
+		if(Input.GetKey(KeyCode.S))
+			transform.position = new Vector3(transform.position.x, transform.position.y - movementSpeed, transform.position.z);
+
+		
+		if(Input.GetKey(KeyCode.A))
+			transform.position = new Vector3(transform.position.x - movementSpeed, transform.position.y, transform.position.z);
+		
+		if(Input.GetKey(KeyCode.D))
+			transform.position = new Vector3(transform.position.x + movementSpeed, transform.position.y, transform.position.z);
+	}
+}
