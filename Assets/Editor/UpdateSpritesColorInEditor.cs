7a99bfcd60ad2da24adb26cf0f17429d34a91fbd
Matt Krystek <mkrystek@gmail.com>
2014-01-24 23:29:31 -0800

    Added more temp lights into the scene Hooked up simple animations on the lights Created a new tint light editor helper script Auto rename the gameobject based on the currently selected light enum

---
+++ Assets/Editor/UpdateSpritesColorInEditor.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+[CanEditMultipleObjects]
+[CustomEditor( typeof(TintSpriteColor))]
+public class UpdateSpritesColorInEditor : Editor 
+{
+	
+	public override void OnInspectorGUI()
+	{
+		base.OnInspectorGUI();
+		var gameObjects = Selection.GetFiltered(typeof(GameObject), SelectionMode.Unfiltered);
+		
+		foreach (GameObject gameObject in gameObjects)
+		{
+			var COLORS = gameObject.GetComponent<TintSpriteColor>().currentColor;
+			bool renderObject = gameObject.GetComponent<TintSpriteColor>().isOn;
+			var spriteRenderer = gameObject.GetComponent<SpriteRenderer>(); 
+			
+			if( spriteRenderer != null)
+			{
+				if(!renderObject)
+				{
+					spriteRenderer.renderer.enabled = false;
+				}
+				else
+					spriteRenderer.renderer.enabled = true;
+
+				switch( COLORS )
+				{
+				case TintSpriteColor.COLORS.Red:
+					spriteRenderer.color = Color.red;
+					break;
+					
+				case TintSpriteColor.COLORS.Yellow:
+					spriteRenderer.color = Color.yellow;
+					break;
+					
+				case TintSpriteColor.COLORS.Blue:
+					spriteRenderer.color = Color.blue;
+					break;
+					
+				case TintSpriteColor.COLORS.Green:
+					spriteRenderer.color = Color.green;
+					break;
+					
+				case TintSpriteColor.COLORS.White:
+					spriteRenderer.color = Color.white;
+					break;
+				}
+
+				gameObject.name = COLORS + " Light";
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/TintSpriteColor.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TintSpriteColor : MonoBehaviour {
+
+	public bool isOn = true;
+	public enum COLORS{White, Red, Blue, Green, Yellow  }
+	public COLORS currentColor;
+}
