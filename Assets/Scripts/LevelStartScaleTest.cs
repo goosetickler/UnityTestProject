2e47513c9cba91a02ab7dc1639eadbf22067d132
Matt Krystek <mkrystek@gmail.com>
2014-01-26 18:08:53 -0800

    Imported the ITween plugin Scale all of the gameobjects in the scene from 0 to the original scale when the game starts

---
+++ Assets/Scripts/LevelStartScaleTest.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelStartScaleTest : MonoBehaviour {
+
+	private GameObject[] levelObjects;
+
+	// Use this for initialization
+	void Start () 
+	{
+		levelObjects = GameObject.FindObjectsOfType(typeof (GameObject)) as GameObject[];
+		Debug.Log ( levelObjects.Length );
+		ScaleGameObjects( levelObjects );
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+
+	void ScaleGameObjects( GameObject[] Objects )
+	{
+		foreach (GameObject obj in Objects) 
+		{
+			Vector3 currentScale = obj.transform.localScale;
+			obj.transform.localScale = new Vector3(0,0,0);
+			StartCoroutine(ResetObjectScale(obj, currentScale));
+		}
+	}
+
+	private IEnumerator ResetObjectScale( GameObject obj, Vector3 scale )
+	{
+		yield return new WaitForSeconds(Random.Range(0.1f, 0.25f));
+		iTween.ScaleTo( obj, scale, 1.5f);
+	}
+}
